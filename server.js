5cd250717d35f2ed897ead418ccf22f5c1406bb5
waseem-medhat <wipdev.tech@gmail.com>
2024-05-28 13:42:25 +0300

    Add GET resources with path and query params

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 import express from "express"
-import path from "path"
-import { fileURLToPath } from "url"
+import dotenv from "dotenv"
+// import path from "path"
+// import { fileURLToPath } from "url"
 
+dotenv.config()
+const port = process.env.PORT || 8080
 const app = express()
 
 // static dir
-const currentDir = path.dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.join(currentDir, "public")))
+// const currentDir = path.dirname(fileURLToPath(import.meta.url))
+// app.use(express.static(path.join(currentDir, "public")))
 
-app.listen(8080, () => console.log(`Server is running on port 8080`))
+let posts = [
+    { id: 1, title: "Hello" },
+    { id: 2, title: "Goodbye" },
+    { id: 3, title: "Hello" },
+    { id: 4, title: "Goodbye" },
+]
+
+app.get('/api/posts', (req, res) => {
+    const limit = parseInt(req.query.limit)
+    res.json(isNaN(limit) ? posts : posts.slice(0, limit))
+})
+
+app.get('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.filter(p => p.id === id)[0]
+    res.json(post)
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
